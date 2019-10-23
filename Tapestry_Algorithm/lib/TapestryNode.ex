28b48b20728ce615b2b4123a344225f76c60a607
MeghaNagarmunoli <meghanagarmunoli@gmail.com>
2019-10-22 20:35:27 -0400

    added nextHop

--- Tapestry_Algorithm/lib/TapestryNode.ex
+++ Tapestry_Algorithm/lib/TapestryNode.ex
@@ -1,3 +1,18 @@
 defmodule TapestryNode do
   use GenServer
+
+  def init(state) do
+    {:ok,
+      %{
+      "routingTable" = Utils.tableInit(length(state["nodeID"])
+      "nodeID" = state["nodeID"]
+      "backPointers" = []
+      }
+    }
+  end
+
+  def handle_cast(:addNode, n) do
+
+  end
+
 end
